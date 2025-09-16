bf9adacb19fae6d51fb388c2c6614db28938ea6f
GabrielKewryghan <g.rosas@aluno.senai.br>
2025-09-16 09:59:33 -0500

    Filmes

---
+++ Aula/script.js
@@ -0,0 +1,21 @@
+let filme = prompt("Digite o nome do filme para verificar a estreia:");
+
+switch (filme) {
+    case "duna 2":
+        alert("Filme: Duna 2 Estreia: 15 de setembro de 2025");
+        break;
+    case "deadpool 3":
+        alert("Filme: Deadpool 3 Estreia: 25 de setembro de 2025");
+        break;
+    case "a freira 2":
+        alert("Filme: A Freira 2 Estreia: 10 de setembro de 2025");
+        break;
+    case "megamind 2":
+        alert("Filme: Megamind 2 Estreia: 18 de setembro de 2025");
+        break;
+    case "avatar 3":
+        alert("Filme: Avatar 3 Estreia: 22 de setembro de 2025");
+        break;
+    default:
+        alert(" Filme fora de estreia.");
+}
